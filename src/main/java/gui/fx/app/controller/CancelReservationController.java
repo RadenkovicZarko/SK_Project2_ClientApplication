5cee255f46d21ed8d86403aef4a19d49334ac602
Zarko Radenkovic <zradenkovic520rn@raf.rs>
2023-01-04 18:51:23 +0100

    finished

--- src/main/java/gui/fx/app/controller/CancelReservationController.java
+++ src/main/java/gui/fx/app/controller/CancelReservationController.java
@@ -3,12 +3,14 @@ package gui.fx.app.controller;
 import gui.fx.app.restclient.ReservationCompanyServiceRest;
 import gui.fx.app.restclient.dtoReservationService.ReservationCancelDto;
 import gui.fx.app.restclient.dtoReservationService.ReservationDto;
-import gui.fx.app.restclient.dtoReservationService.VehicleDto;
+
 import gui.fx.app.view.ReservationView;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
 
 import java.io.IOException;
+import java.util.Date;
 
 public class CancelReservationController implements EventHandler<ActionEvent> {
     private ReservationView reservationView;
@@ -23,6 +25,14 @@ public class CancelReservationController implements EventHandler<ActionEvent> {
     public void handle(ActionEvent event) {
         ReservationDto selected = reservationView.getUserReservationtable().getSelectionModel().getSelectedItem();
 
+        Date date=new Date();
+        if(selected.getDate_from().getTime()<date.getTime())
+        {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setContentText("You cannot cancel a reservation that has passed");
+            alert.showAndWait();
+            return;
+        }
         ReservationCancelDto reservationCancelDto=new ReservationCancelDto();
         reservationCancelDto.setId(selected.getId());
         reservationCancelDto.setId_vehicle(selected.getId_vehicle());
